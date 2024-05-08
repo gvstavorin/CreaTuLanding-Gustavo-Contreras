d60cb7a997824eb0a82e910cedd35d565633bace
gvstavorin <gustavoandreescg@gmail.com>
2024-05-08 17:59:48 -0400

    se agrego new promise a la funciones

--- src/utils/MockData.js
+++ src/utils/MockData.js
@@ -1,34 +1,45 @@
 
-  export const getProducts = () => {
-    return fetch("https://fakestoreapi.com/products").then((res) => res.json());
+ export const getProducts = () => {
+  return new Promise((resolve, reject) => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => {
+        if (!response.ok) {
+          reject(new Error(`Error en la respuesta de la API: ${response.status}`));
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => resolve(data))
+      .catch((error) => {
+        console.error('Error al obtener los productos:', error);
+        reject(error);
+      });
+  });
+};
 
+export const getProductsByCategory = (categoriaId) => {
+  const categoryMap = {
+    electronica: 'electronics',
+    mens: "men's clothing",
+    womens: "women's clothing",
+    joyeria: 'jewelery',
   };
 
-  export const getProductsByCategory = (categoriaId) => {
-    if (categoriaId==='electronica'){
-        categoriaId = 'electronics';
-    }else if(categoriaId==='mens'){
-      categoriaId = "men's clothing";
-    }else if(categoriaId==='womens'){
-      categoriaId = "women's clothing";
-    }else if(categoriaId==='joyeria'){
-      categoriaId = 'jewelery';
-    } 
-    return fetch('https://fakestoreapi.com/products/category/'+categoriaId).then((res) => res.json());
+  const apiCategory = categoryMap[categoriaId] || categoriaId;
 
-  };
-  
-  export const setProduct = () => {
-    return fetch("https://fakestoreapi.com/products", {
-      method: "POST",
-      body: JSON.stringify({
-        title: "test product",
-        price: 13.5,
-        description: "lorem ipsum set",
-        image: "https://i.pravatar.cc",
-        category: "electronic",
-      }),
-    }).then((res) => res.json());
-  
-
-  };
+  return new Promise((resolve, reject) => {
+    fetch(`https://fakestoreapi.com/products/category/${apiCategory}`)
+      .then((response) => {
+        if (!response.ok) {
+          return reject(new Error(`Error en la API: ${response.statusText}`));
+        }
+        return response.json();
+      })
+      .then((data) => resolve(data))
+      .catch((error) => {
+        console.error('Error al obtener los productos:', error);
+        reject(error);
+      });
+  });
+};
+ 
