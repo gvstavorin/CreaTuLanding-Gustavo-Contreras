98195286f5419579a3aae494a70a630b65dc8a5c
gvstavorin <gustavoandreescg@gmail.com>
2024-05-31 19:45:05 -0400

    env agregado

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdL1S7PRHvTrcfjhElW5vYr5ENR6fWmEw",
-  authDomain: "store-mng.firebaseapp.com",
-  projectId: "store-mng",
-  storageBucket: "store-mng.appspot.com",
-  messagingSenderId: "618557175547",
-  appId: "1:618557175547:web:662e74495625e7e0bca25e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
